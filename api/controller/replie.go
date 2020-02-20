7232ade686e75b097689eabf2e7286d27acc6204
Christopher Delgado <Christopherdel98@gmail.com>
2020-02-19 17:44:20 -0700

    [DISC-2] Update for threads and full back of the replies entities

---
+++ api/controller/replie.go
@@ -0,0 +1,100 @@
+package controller
+
+import (
+	"net/http"
+	"app/model"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"strconv"
+)
+
+//Get all replies of a thread
+func GetRepliesByThread(w http.ResponseWriter, r *http.Request) {
+	var replie model.Replie
+
+	params := mux.Vars(r)
+
+	threadId := params["thread_id"]
+
+	threadIdInt, _ := strconv.Atoi(threadId)
+
+	var output []*model.Replie
+	output = replie.GetAllReplies(threadIdInt)
+
+	w.Header().Set(CONTENT_TYPE, APPLICATION_JSON)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(output)
+}
+
+//Get single thread
+func GetReplie(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	replieId := params["id"]
+
+	var output model.Replie
+	output = output.GetReplie(replieId)
+
+	w.Header().Set(CONTENT_TYPE, APPLICATION_JSON)
+
+	if output.ID == 0 {
+		m := "No replie found"
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(m)
+	} else {
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(output)
+	}
+}
+
+//Create replie
+func CreateReplie(w http.ResponseWriter, r *http.Request) {
+	var replie model.Replie
+
+	json.NewDecoder(r.Body).Decode(&replie)
+
+	replie.CreateReplie()
+
+	w.Header().Set(CONTENT_TYPE, APPLICATION_JSON)
+
+	if replie.ID != 0 {
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(replie)
+	}else{
+		m := "Fail in replie creation"
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(m)
+	}
+}
+
+//Update replie
+func UpdateReplie(w http.ResponseWriter, r *http.Request) {
+	var replie model.Replie
+
+	json.NewDecoder(r.Body).Decode(&replie)
+
+	params := mux.Vars(r)
+
+	replieId := params["id"]
+
+	replie.ID, _ = strconv.Atoi(replieId)
+
+	replie.UpdateReplie()
+
+	w.Header().Set(CONTENT_TYPE, APPLICATION_JSON)
+
+	if replie.ID != 0 {
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(replie)
+	}else{
+		m := "Fail in replie update"
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(m)
+	}
+}
+
+//Delete replie
+func DeleteReplie(w http.ResponseWriter, r *http.Request) {
+	//Missing implementation
+}
+
--- api/controller/thread.go
+++ api/controller/thread.go
@@ -2,10 +2,10 @@ package controller
 
 import (
 	"net/http"
-	"fmt"
 	"app/model"
 	"encoding/json"
 	"github.com/gorilla/mux"
+	"strconv"
 )
 const CONTENT_TYPE  = "content-type"
 const APPLICATION_JSON = "application/json"
@@ -22,7 +22,7 @@ func GetThreads(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(output)
 }
 
-//Get all threads
+//Get single thread
 func GetThread(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 
@@ -43,7 +43,7 @@ func GetThread(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-//Get all threads
+//Create thread
 func CreateThread(w http.ResponseWriter, r *http.Request) {
 	var thread model.Thread
 
@@ -63,13 +63,34 @@ func CreateThread(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-//Get all threads
+//Update thread
 func UpdateThread(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello world</h1>") //Missing implementation
+	var thread model.Thread
+
+	json.NewDecoder(r.Body).Decode(&thread)
+
+	params := mux.Vars(r)
+
+	threadId := params["id"]
+
+	thread.ID, _ = strconv.Atoi(threadId)
+
+	thread.UpdateThread()
+
+	w.Header().Set(CONTENT_TYPE, APPLICATION_JSON)
+
+	if thread.ID != 0 {
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(thread)
+	}else{
+		m := "Fail in thread update"
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(m)
+	}
 }
 
-//Get all threads
+//Delete thread
 func DeleteThread(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello world</h1>") //Missing implementation
+	//Missing implementation
 }
 
