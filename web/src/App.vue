79be5039119b5610cf19dbf771f4bdc926791bcc
Luis Sanchez <lsjaquez22@gmail.com>
2020-02-20 15:43:39 -0700

    show comments

--- web/src/App.vue
+++ web/src/App.vue
@@ -15,7 +15,9 @@
         </div>
       </div>
       <div class="column menuTweets">
-        <div class="sectionTitle"><p>Home</p></div>
+        <div class="sectionTitle">
+          <p>Home</p>
+        </div>
         <div class="columns sectionTweets">
           <div class="tweet newTweet">
             <div class="column is-2 avatar">
@@ -41,17 +43,11 @@
                   :disabled="sendTweet"
                   v-on:click="saveTweet()"
                   class="button is-link is-rounded"
-                >
-                  Tweet
-                </button>
+                >Tweet</button>
               </div>
             </div>
           </div>
-          <div
-            v-for="tweet in tweets"
-            v-bind:key="tweet.thread_id"
-            class="tweet"
-          >
+          <div v-for="tweet in tweets" v-bind:key="tweet.thread_id" class="tweet">
             <div class="column is-2 avatar">
               <i class="far fa-user-circle fa-4x"></i>
             </div>
@@ -65,12 +61,10 @@
               </div>
               <div class="messageTweet">
                 <p class="titleTweet">{{ tweet.title }}</p>
-                <p>
-                  {{ tweet.description }}
-                </p>
+                <p>{{ tweet.description }}</p>
               </div>
               <div class="sectionIcons">
-                <i class="far fa-comment fa-1x"></i>
+                <i class="far fa-comment fa-1x" v-on:click="openComments(tweet)"></i>
               </div>
             </div>
           </div>
@@ -78,9 +72,7 @@
       </div>
       <div class="column is-3 trends">
         <article class="panel is-primary">
-          <p class="panel-heading">
-            Trends for you
-          </p>
+          <p class="panel-heading">Trends for you</p>
           <a class="panel-block">
             <div>
               <p class="subTitle">Trending</p>
@@ -133,10 +125,60 @@
           </div>
         </section>
         <footer class="modal-card-foot">
-          <button class="button is-success" v-on:click="updateTweet()">
-            Save changes
-          </button>
+          <button class="button is-success" v-on:click="updateTweet()">Save changes</button>
           <button class="button" v-on:click="closeModal()">Cancel</button>
+        </footer>
+      </div>
+    </div>
+    <div class="modal modalComments" :class="{ 'is-active': showComments }">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <i class="fas fa-times fa-2x" v-on:click="closeComments()"></i>
+        </header>
+        <section class="modal-card-body">
+          <div class="columns tweet newTweet">
+            <div class="column is-2 avatar">
+              <i class="far fa-user-circle fa-4x"></i>
+            </div>
+            <div class="column infoTweet">
+              <div class="messageTweet">
+                <p class="titleTweet">{{ selectedTweet.title }}</p>
+                <p>{{ selectedTweet.description }}</p>
+              </div>
+            </div>
+          </div>
+          <div class="columns" v-for="comment in comments" v-bind:key="comment.replie_id">
+            <div class="column is-1"></div>
+            <div class="column comentarioTweet">
+              <p :id="comment.replie_id">{{ comment.content }}</p>
+              <input
+                :id="comment.replie_id + 'editMode'"
+                class="input titleTweet"
+                style="display:none"
+                type="text"
+                v-model="comment.content"
+              />
+            </div>
+            <div class="column is-1">
+              <i class="fas fa-pen" v-if="editMode == false" v-on:click="openEditMode(comment)"></i>
+            </div>
+          </div>
+        </section>
+        <footer class="modal-card-foot">
+          <input
+            v-if="editMode == false"
+            class="input titleTweet"
+            type="text"
+            placeholder="Reply"
+            v-model="newReply"
+          />
+          <button v-if="editMode == false" class="button is-success" v-on:click="saveReply()">Reply</button>
+
+          <div v-if="editMode == true">
+            <button class="button is-success" v-on:click="updateTweet()">Save changes</button>
+            <button class="button" v-on:click="exitEditMode()">Cancel</button>
+          </div>
         </footer>
       </div>
     </div>
@@ -160,6 +202,14 @@ export default {
       comments: [],
       newTweet: "",
       showModal: false,
+      showComments: false,
+      editMode: false,
+      newReply: "",
+      selectedTweet: {
+        id: 0,
+        title: "",
+        description: ""
+      },
       newTweet: {
         title: "",
         description: ""
@@ -189,6 +239,13 @@ export default {
     }
   },
   methods: {
+    exitEditMode() {
+      this.editMode = false;
+    },
+    openEditMode(comment) {
+      console.log(comment);
+      this.editMode = true;
+    },
     openModal(tweet) {
       this.showModal = true;
       this.editedTweet.id = tweet.thread_id;
@@ -201,27 +258,73 @@ export default {
       this.editedTweet.title = "";
       this.editedTweet.description = "";
     },
-    saveTweet() {
-      // const form = new FormData();
-      // form.append("title", this.newTweet.title);
-      // form.append("description", this.newTweet.description);
-      // axios.post("http://localhost:8081/api/threads", form);
-      // axios.get("http://localhost:8081/api/threads").then(response => {
-      //   this.tweets = response.data.reverse();
-      // });
+    openComments(tweet) {
+      this.showComments = true;
+      this.selectedTweet.id = tweet.thread_id;
+      this.selectedTweet.title = tweet.title;
+      this.selectedTweet.description = tweet.description;
+      axios
+        .get(`http://localhost:8081/api/replies/thread/${tweet.thread_id}`)
+        .then(response => {
+          // JSON responses are automatically parsed.
+
+          this.comments = response.data.reverse();
+        });
+      // this.editedTweet.id = tweet.thread_id;
+      // this.editedTweet.title = tweet.title;
+      // this.editedTweet.description = tweet.description;
     },
-    updateTweet() {
-      // const form = new FormData();
-      // form.append("thread_id", this.editedTweet.thread_id);
-      // form.append("title", this.editedTweet.title);
-      // form.append("description", this.editedTweet.description);
-      // axios.put("http://localhost:8081/api/threads", form);
-      // axios.get("http://localhost:8081/api/threads").then(response => {
-      //   this.tweets = response.data.reverse();
-      // });
+    closeComments() {
+      this.showComments = false;
+      this.newReply = "";
       // this.editedTweet.id = 0;
       // this.editedTweet.title = "";
       // this.editedTweet.description = "";
+    },
+    saveTweet() {
+      const form = new FormData();
+      form.append("title", this.newTweet.title);
+      form.append("description", this.newTweet.description);
+      axios.post("http://localhost:8081/api/threads", form).then(response => {
+        axios.get("http://localhost:8081/api/threads").then(response => {
+          this.tweets = response.data.reverse();
+          this.newTweet.title = "";
+          this.newTweet.description = "";
+          alert("Thread created sucessfully!");
+        });
+      });
+    },
+    updateTweet() {
+      const form = new FormData();
+      form.append("title", this.editedTweet.title);
+      form.append("description", this.editedTweet.description);
+      let id = this.editedTweet.id;
+      axios
+        .post("http://localhost:8081/api/threads/" + id, form)
+        .then(response => {
+          axios.get("http://localhost:8081/api/threads").then(response => {
+            this.tweets = response.data.reverse();
+            this.editedTweet.id = 0;
+            this.editedTweet.title = "";
+            this.editedTweet.description = "";
+            this.closeModal();
+          });
+        });
+    },
+    saveReply() {
+      console.log(this.newReply);
+      // const form = new FormData();
+      // form.append("threadParent_id", this.selectedTweet.thread_id);
+      // form.append("content", this.newReply);
+      // axios.post("http://localhost:8081/api/replies", form);
+      // axios
+      //   .get(`http://localhost:8081/api/replies/thread/${this.selectedTweet.thread_id}`)
+      //   .then(response => {
+      //     this.comments = response.data.reverse();
+      //   });
+      //  this.closeComments()()
+      //  ----> Si se borra la informacion antes de hacer el get, se puede ocultar solo el modal
+      // this.showComments = false;
     }
   }
 };
@@ -443,5 +546,47 @@ export default {
 }
 #app .modal-card-foot .is-success {
   background-color: rgba(29, 161, 242, 1);
+}
+
+.modalComments .column {
+  border: 0px;
+}
+
+.modalComments .messageTweet p {
+  text-align: left;
+}
+
+.modalComments .comentarioTweet {
+  text-align: left;
+}
+.modalComments .comentarioTweet p {
+  border-top: 1px solid rgb(56, 68, 77);
+  padding: 15px 0;
+  width: 100%;
+  font-size: 18px;
+  color: white;
+}
+
+.modalComments i {
+  padding: 15px 0;
+  color: white;
+}
+
+.modalComments i:hover {
+  cursor: pointer;
+}
+
+.modalComments .modal-card-foot input {
+  background-color: transparent;
+  border-top-style: hidden;
+  border-right-style: hidden;
+  border-left-style: hidden;
+  border-bottom-style: hidden;
+  color: white;
+}
+
+.modalComments .modal-card-foot input::placeholder {
+  color: rgb(56, 68, 77);
+  font-weight: bold;
 }
 </style>
